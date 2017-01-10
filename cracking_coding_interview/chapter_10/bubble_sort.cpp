ebbc70bbe03693372d404853ae8acb93e4cbb693
Bruno Roy <brusi_roy@hotmail.com>
2017-01-10 16:37:31 -0500

    Cracking the Coding Interview Selection Sort

--- cracking_coding_interview/chapter_10/bubble_sort.cpp
+++ cracking_coding_interview/chapter_10/bubble_sort.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <climits>
 #include <vector>
 
 void swap(std::vector<int>& v, size_t index1, size_t index2) {
@@ -20,6 +21,21 @@ void bubbleSort(std::vector<int>& v) {
   } while (swapped);
 }
 
+void selectionSort(std::vector<int>& v) {
+  for (auto i = 0; i < v.size(); ++i) {
+    std::pair<int, int> index_min = std::make_pair(-1, INT_MAX);
+    for (auto j = i + 1; j < v.size(); ++j) {
+      if (v[j] < index_min.second && v[j] < v[i]) {
+        index_min.first = j;
+        index_min.second = v[j];
+      }
+    }
+    if (index_min.first != -1) {
+      swap(v, i, index_min.first);
+    }
+  }
+}
+
 int main() {
 
   auto v = std::vector<int>();
@@ -33,7 +49,8 @@ int main() {
   }
 
   std::cout << "Sorting" << std::endl;
-  bubbleSort(v);
+  // bubbleSort(v);
+  selectionSort(v);
 
   for(auto x : v) {
     std::cout << x << std::endl;
