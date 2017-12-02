7f3ca4fe2d18355090bc5b0feb52bf4acdd8f11b
Bruno Roy <brusi_roy@hotmail.com>
2017-12-02 13:13:33 -0500

    Advent of Code 2017 day 2, puzzle 2

--- advent_of_code/2017/day_2/day_2.py
+++ advent_of_code/2017/day_2/day_2.py
@@ -14,4 +14,22 @@ def part_1(input_file_name):
 
         return ret_sum
 
+def part_2(input_file_name):
+    """ Part 2 """
+    with open(input_file_name) as f:
+        ret_sum = 0
+        for line in f:
+            line_list = [int(n) for n in line.split()]
+            list_size = len(line_list)
+            for i in range(list_size):
+                for j in range(i + 1, list_size):
+                    if line_list[i] % line_list[j] == 0:
+                        ret_sum += line_list[i] // line_list[j]
+                    elif line_list[j] % line_list[i] == 0:
+                        ret_sum += line_list[j] // line_list[i]
+
+        return ret_sum
+
+
 print("Part one: {}".format(part_1(INPUT_FILE_NAME)))
+print("Part two: {}".format(part_2(INPUT_FILE_NAME)))
