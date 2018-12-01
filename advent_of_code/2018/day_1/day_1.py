dcb123d74388794d8b95512f13b5ceea2b416f6f
Bruno Roy <brusi_roy@hotmail.com>
2018-12-01 16:57:21 -0500

    [AoC-2018] Day 1, Puzzle 2

--- advent_of_code/2018/day_1/day_1.py
+++ advent_of_code/2018/day_1/day_1.py
@@ -8,4 +8,25 @@ def part_1():
                 total += int(line[1:])
     return total
 
-print('The total is: ', part_1())
+def part_2():
+    lines = []
+    with open('input.txt') as f:
+        lines = f.readlines()
+
+    total = 0
+    frequencies = set()
+    while True:
+        for line in lines:
+            if line.startswith('-'):
+                total -= int(line[1:])
+            else:
+                total += int(line[1:])
+
+            if total in frequencies:
+                return total
+            else:
+                frequencies.add(total)
+
+
+print('Part 1 -> The total is: ', part_1())
+print('Part 2 -> The firs frequency hit twice is: ', part_2())
