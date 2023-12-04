a76279a60f85ae4f9d14f0488d3004c4ac640c7d
Bruno Roy <brusi_roy@hotmail.com>
2023-12-03 18:03:11 -0800

    [AoC 2023] Day 2, Puzzle 1 & 2

--- advent_of_code/2023/app/Day2.hs
+++ advent_of_code/2023/app/Day2.hs
@@ -2,52 +2,46 @@ import Text.Regex.TDFA ((=~))
 import Data.List (isInfixOf)
 import Data.List.Split (splitOn)
 import Data.List (group)
+import Data.Char (isDigit)
 
 data ColorsPerGame = RGB { red :: Int, green :: Int, blue :: Int }
     deriving (Show)
 
--- interpretCubes :: [String] -> ColorsPerGame
--- interpretCubes cubes = 
---   -- for each set in the game, we pick the max
---   RGB { red = 255, green = 0, blue = 128 }
+extractGameIdAndRest :: String -> (Int, ColorsPerGame)
+extractGameIdAndRest line =
+  let
+    (_, _, cubes, gameId) = line =~ "Game ([[:digit:]]+):" :: (String, String, String, [String])
+    gameId' = read $ head gameId :: Int
+    formattedCubes = cubes =~ "[[:digit:]]+ [a-z]+" :: [[String]]
+    colors = interpretCubes $ concat formattedCubes
+  in
+    (gameId', colors)
 
-interpretCubes :: [String] -> ([Int], [Int], [Int])
-interpretCubes xs = (red, green, blue)
+getMaxForColor :: String -> [String] -> Int
+getMaxForColor color xs = maximum $ map (read . takeWhile (\ c -> isDigit c)) $ filter (isInfixOf color) xs
+
+interpretCubes :: [String] -> ColorsPerGame
+interpretCubes xs = RGB red green blue
   where
-    red   =  group $ takeWhile (\c -> isDigit c) . filter (isInfixOf "red")  xs :: Int
-    green = filter (isInfixOf "green") xs
-    blue  = filter (isInfixOf "blue") xs
+    red   = getMaxForColor "red" xs
+    green = getMaxForColor "green" xs
+    blue  = getMaxForColor "blue" xs
+
+-- Used for Part 1
+isPossible :: (Int, ColorsPerGame) -> Int
+isPossible (gameId, colors) = 
+  if red colors <= 12 &&  green colors <= 13 && blue colors <= 14
+    then gameId
+    else 0
+
+-- Used for Part 2
+powerFewestCubes :: (Int, ColorsPerGame) -> Int
+powerFewestCubes (gameId, colors) = red colors * green colors * blue colors
+
 
 main :: IO ()
 main = do
   input <- readFile "./inputs/day2.input"
   let input' = lines input
-  
-  let first = input' !! 0
-  print first
-  
-  -- Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
-  -- let regexPattern = "Game ([[:digit:]]+): ([[:digit:]]+ [a-z]+[, ]?)+"
-  let regexPattern = "Game ([[:digit:]]+):"
-
-  let (_, _, cubesAllPulls, gameId) = first =~ regexPattern :: (String, String, String, [String])
-  let gameId' = head gameId
-  putStrLn ("Game ID:" ++ gameId')
-  let cubesAllPulls' = splitOn ";" cubesAllPulls
-  let headCubes = head cubesAllPulls'
-  let matches = cubesAllPulls =~ "[[:digit:]]+ [a-z]+" :: [[String]]
-  print matches
-
-  -- -- The regex pattern with a group capturing the date part
-  -- let regexPattern = "Date: (\\d{4}-\\d{2}-\\d{2})"
-  
-  -- -- Applying the regex and extracting the captured group
-  -- let matchResult = inputString =~ regexPattern :: (String, String, String, [String])
-  
-  -- -- The fourth element of the tuple contains the captured groups
-  -- let capturedGroups = if length (snd matchResult) > 0 then head (snd matchResult) else ""
-  
-  -- putStrLn $ "Input String: " ++ inputString
-  -- putStrLn $ "Captured Date: " ++ capturedGroups
-
-  -- print input'
+  putStrLn $ "Part 1: " ++ show (sum $ map (isPossible . extractGameIdAndRest) input')
+  putStrLn $ "Part 2: " ++ show (sum $ map (powerFewestCubes . extractGameIdAndRest) input')
