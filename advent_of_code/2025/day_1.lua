1f47774ea22680bb0f2f08d7c8a9570ffa699765
Bruno Roy <brunoroy1024@gmail.com>
2025-12-01 14:46:28 -0500

    [AoC 2025] Day 1, Puzzle 2

--- advent_of_code/2025/day_1.lua
+++ advent_of_code/2025/day_1.lua
@@ -1,22 +1,57 @@
-local input_file = io.open("day_1.input", "r")
+local function part_1(content)
+	local current_dial = 50
+	local number_of_zeros = 0
 
-local current_dial = 50
-local number_of_zeros = 0
+	for line in content:gmatch("[^\n]+") do
+		local direction = string.sub(line, 1, 1)
+		local amount = tonumber(string.sub(line, 2))
+		if direction == "R" then
+			current_dial = current_dial + amount
+		else
+			current_dial = current_dial - amount
+		end
 
-for line in input_file:lines() do
-	local direction = string.sub(line, 1, 1)
-	local amount = tonumber(string.sub(line, 2))
-	if direction == "R" then
-		current_dial = current_dial + amount
-	else
-		current_dial = current_dial - amount
+		if current_dial % 100 == 0 then
+			number_of_zeros = number_of_zeros + 1
+		end
 	end
-
-	if current_dial % 100 == 0 then
-		number_of_zeros = number_of_zeros + 1
-	end
+	return number_of_zeros
 end
 
-print("Part 1: ", number_of_zeros)
+local function part_2(content)
+	local current_dial = 50
+	local passing_zero = 0
 
+	for line in content:gmatch("[^\n]+") do
+		local direction = string.sub(line, 1, 1)
+		local amount = tonumber(string.sub(line, 2))
+
+		if direction == "R" then
+			current_dial = current_dial + amount
+			if current_dial > 99 then
+				passing_zero = passing_zero + (current_dial // 100)
+				current_dial = current_dial % 100
+			end
+		else
+			current_dial = current_dial - amount
+			if current_dial == 0 then
+				passing_zero = passing_zero + 1
+			end
+			if current_dial < 0 and current_dial + amount ~= 0 then
+				passing_zero = passing_zero + (math.abs(current_dial) // 100) + 1
+			end
+			if current_dial < 0 and current_dial + amount == 0 then
+				passing_zero = passing_zero + (math.abs(current_dial) // 100)
+			end
+			current_dial = current_dial % 100
+		end
+	end
+	return passing_zero
+end
+
+local input_file = io.open("day_1.input", "r")
+local content = input_file:read("*all")
 input_file:close()
+
+print("Part 1: ", part_1(content))
+print("Part 2: ", part_2(content))
