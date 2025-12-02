be17bdde748f42ad325753233af57c8cb7a82e86
Bruno Roy <brunoroy1024@gmail.com>
2025-12-02 09:13:24 -0500

    [AoC 2025] Day 2, Cleanup

--- advent_of_code/2025/day_2.lua
+++ advent_of_code/2025/day_2.lua
@@ -1,40 +1,28 @@
-local function part_1(content)
-	local sum = 0
+local function part_1_2(content)
+	local part_1, part_2 = 0, 0
 	for start_index, end_index in content:gmatch("(%d+)-(%d+)") do
 		for i = tonumber(start_index), end_index do
 			local i_str = tostring(i)
-			if i_str:len() % 2 == 0 then
-				local middle = i_str:len() / 2
-				if i_str:sub(1, middle) == i_str:sub(middle + 1) then
-					sum = sum + tonumber(i_str)
-				end
+			local middle = i_str:len() // 2
+			if i_str:sub(1, middle) == i_str:sub(middle + 1) then
+				part_1 = part_1 + i
 			end
-		end
-	end
-	return sum
-end
-
-local function part_2(content)
-	local sum = 0
-	for start_index, end_index in content:gmatch("(%d+)-(%d+)") do
-		for i = tonumber(start_index), end_index do
-			local i_str = tostring(i)
-			local middle = i_str:len() / 2
 			for c = 1, middle do
 				local sub = i_str:sub(1, c)
 				if i_str:gsub(sub, ""):len() == 0 then
-					sum = sum + tonumber(i_str)
+					part_2 = part_2 + i
 					break
 				end
 			end
 		end
 	end
-	return sum
+	return part_1, part_2
 end
 
 local input_file = io.open("day_2.input", "r")
 local content = input_file:read("*all")
 input_file:close()
 
-print("Part 1: ", part_1(content))
-print("Part 2: ", part_2(content))
+local part_1, part_2 = part_1_2(content)
+print("Part 1: ", part_1)
+print("Part 2: ", part_2)
