9c7f91b272db109ac2d9f98793b0ed2d1c9fe6f6
Bruno Roy <brunoroy1024@gmail.com>
2025-12-03 11:45:20 -0500

    [AoC 2025] Day 3, Cleanup

--- advent_of_code/2025/day_3.lua
+++ advent_of_code/2025/day_3.lua
@@ -11,25 +11,13 @@ local function find_max_char(str)
 	return max, max_index
 end
 
-local function part_1(content)
-	local joltage = 0
-	for line in content:gmatch("[^%s]+") do
-		-- ignore the last character
-		local first_digit = find_max_char(line:sub(1, -2))
-		local second_digit = find_max_char(line:sub(line:find(first_digit, 1, true) + 1))
-		joltage = joltage + first_digit * 10
-		joltage = joltage + second_digit
-	end
-	return joltage
-end
-
-local function part_2(content)
+local function part_1_2(content, length)
 	local joltage = 0
 	for line in content:gmatch("[^%s]+") do
 		local digits = {}
 		local start_index = 1
 
-		for i = -12, -1, 1 do
+		for i = -length, -1, 1 do
 			local digit, digit_index = find_max_char(line:sub(start_index, i))
 			table.insert(digits, digit)
 			start_index = start_index + digit_index
@@ -43,5 +31,5 @@ local input_file = io.open("day_3.input", "r")
 local content = input_file:read("*all")
 input_file:close()
 
-print("Part 1: ", part_1(content))
-print("Part 2: ", part_2(content))
+print("Part 1: ", part_1_2(content, 2))
+print("Part 2: ", part_1_2(content, 12))
