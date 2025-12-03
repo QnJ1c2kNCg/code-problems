aa3ee3dc7719d137d1f6e50b02aac676ff7a63b3
Bruno Roy <brunoroy1024@gmail.com>
2025-12-03 11:40:40 -0500

    [AoC 2025] Day 3, Puzzle 2

--- advent_of_code/2025/day_3.lua
+++ advent_of_code/2025/day_3.lua
@@ -1,11 +1,14 @@
 local function find_max_char(str)
 	local max = "0"
-	for char in str:gmatch(".") do
+	local max_index = 1
+	for i = 1, #str do
+		local char = str:sub(i, i)
 		if char > max then
 			max = char
+			max_index = i
 		end
 	end
-	return max
+	return max, max_index
 end
 
 local function part_1(content)
@@ -20,8 +23,25 @@ local function part_1(content)
 	return joltage
 end
 
+local function part_2(content)
+	local joltage = 0
+	for line in content:gmatch("[^%s]+") do
+		local digits = {}
+		local start_index = 1
+
+		for i = -12, -1, 1 do
+			local digit, digit_index = find_max_char(line:sub(start_index, i))
+			table.insert(digits, digit)
+			start_index = start_index + digit_index
+		end
+		joltage = joltage + table.concat(digits)
+	end
+	return joltage
+end
+
 local input_file = io.open("day_3.input", "r")
 local content = input_file:read("*all")
 input_file:close()
 
 print("Part 1: ", part_1(content))
+print("Part 2: ", part_2(content))
