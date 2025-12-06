91a85d13025ca2975fa8220c2868a83d0923f2b7
Bruno Roy <brunoroy1024@gmail.com>
2025-12-06 10:55:00 -0500

    [AoC 2025] Day 6, Puzzle 2
    Quite ugly...

--- advent_of_code/2025/day_6.lua
+++ advent_of_code/2025/day_6.lua
@@ -45,8 +45,49 @@ local function part_1(content)
 	return grand_total
 end
 
+local function part_2(content)
+	local n_cols = content:find("\n") - 1
+	local _, n_rows = content:gsub("\n", "\n")
+
+	local grouped_cols = {}
+	local current_grouped_cols = {}
+	for col = 1, n_cols do
+		local row_value = {}
+		for row = 1, n_rows - 1 do
+			local coords = col + (row - 1) * (n_cols + 1)
+			table.insert(row_value, content:sub(coords, coords))
+		end
+
+		local number = tonumber(table.concat(row_value))
+		if number then
+			table.insert(current_grouped_cols, number)
+		else
+			-- This is the case where it's all whitespace
+			table.insert(grouped_cols, current_grouped_cols)
+			current_grouped_cols = {}
+		end
+	end
+	table.insert(grouped_cols, current_grouped_cols)
+
+	local operators = {}
+	for operator in content:gmatch("[%+%*]") do
+		table.insert(operators, operator)
+	end
+
+	assert(#grouped_cols == #operators)
+
+	local grand_total = 0
+	for i = 1, #grouped_cols do
+		grand_total = grand_total + reduce_numbers(grouped_cols[i], operators[i])
+	end
+	return grand_total
+end
+
 local content = utils.read_file("day_6.input")
 
 utils.time(function()
 	print("Part 1: ", part_1(content))
+end)
+utils.time(function()
+	print("Part 2: ", part_2(content))
 end)
