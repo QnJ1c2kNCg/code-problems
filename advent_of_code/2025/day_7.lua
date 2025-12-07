db4178e28bc69e6d6a210d73f154f0a03501ac87
Bruno Roy <brunoroy1024@gmail.com>
2025-12-07 12:54:56 -0500

    [AoC 2025] Day 7, Puzzle 2

--- advent_of_code/2025/day_7.lua
+++ advent_of_code/2025/day_7.lua
@@ -9,13 +9,9 @@ local function part_1(content)
 		local new_beam_locations = {}
 		for _, beam_location in pairs(beam_locations) do
 			if line:sub(beam_location, beam_location) == "^" then
-				if beam_location > 1 then
-					new_beam_locations[beam_location - 1] = beam_location - 1
-				end
-				-- TODO: maybe there should be a -1 here
-				if beam_location < #line then
-					new_beam_locations[beam_location + 1] = beam_location + 1
-				end
+				-- It's impossible to out of bound with the input
+				new_beam_locations[beam_location - 1] = beam_location - 1
+				new_beam_locations[beam_location + 1] = beam_location + 1
 				beam_locations[beam_location] = nil
 				n_splits = n_splits + 1
 			end
@@ -27,8 +23,43 @@ local function part_1(content)
 	return n_splits
 end
 
+local function part_2(content)
+	local function recurse_inner(beam, lines, line_index, dp)
+		local key = tostring(beam) .. "|" .. tostring(line_index)
+		if dp[key] then
+			return dp[key]
+		end
+		-- Reached the end
+		if not lines[line_index] then
+			return 1
+		end
+
+		local out = nil
+		if lines[line_index]:sub(beam, beam) == "^" then
+			out = recurse_inner(beam - 1, lines, line_index + 1, dp)
+				+ recurse_inner(beam + 1, lines, line_index + 1, dp)
+		else
+			out = recurse_inner(beam, lines, line_index + 1, dp)
+		end
+
+		dp[key] = out
+		return out
+	end
+
+	local dp = {}
+	local lines = {}
+	for line in content:gmatch("[^%s]+") do
+		table.insert(lines, line)
+	end
+
+	return recurse_inner(content:find("S"), lines, 2, dp)
+end
+
 local content = utils.read_file("day_7.input")
 
 utils.time(function()
 	print("Part 1: ", part_1(content))
+end)
+utils.time(function()
+	print("Part 2: ", part_2(content))
 end)
