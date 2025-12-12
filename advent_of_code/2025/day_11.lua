5918a1707a660e5ffe4e11f85e10eb077ca67381
Bruno Roy <brunoroy1024@gmail.com>
2025-12-12 10:38:29 -0500

    [AoC 2025] Day 11, Puzzle 2

--- advent_of_code/2025/day_11.lua
+++ advent_of_code/2025/day_11.lua
@@ -12,7 +12,7 @@ local function create_graph(content)
 	return graph
 end
 
-local function visit(graph, node, visited_nodes)
+local function visit1(graph, node, visited_nodes)
 	if node == "out" then
 		return 1
 	end
@@ -23,21 +23,64 @@ local function visit(graph, node, visited_nodes)
 
 	-- Visit all the neighbords
 	local paths = 0
-	visited_nodes[node] = true
 	for _, neigh in ipairs(graph[node]) do
-		paths = paths + visit(graph, neigh, visited_nodes)
+		paths = paths + visit1(graph, neigh, visited_nodes)
 	end
 	visited_nodes[node] = false
 	return paths
 end
 
-local function part_1(content)
-	local graph = create_graph(content)
-	return visit(graph, "you", {})
+local function part_1(graph)
+	return visit1(graph, "you", {})
+end
+
+local function visit2(graph, node, cache, pois)
+	-- The cache is just memoization
+	-- We can just have the visited nodes cause we might have cycles
+	local key = node .. table.concat(pois)
+	if cache[key] then
+		return cache[key]
+	end
+
+	if node == "out" then
+		if #pois == 0 then
+			return 1
+		else
+			return 0
+		end
+	end
+
+	local removed = 0
+	for i, v in ipairs(pois) do
+		if node == v then
+			removed = i
+			table.remove(pois, i)
+			break
+		end
+	end
+
+	-- Visit all the neighbords
+	local paths = 0
+	for _, neigh in ipairs(graph[node]) do
+		paths = paths + visit2(graph, neigh, cache, pois)
+	end
+	cache[key] = paths
+	if removed > 0 then
+		table.insert(pois, removed, node)
+	end
+	return paths
+end
+
+local function part_2(graph)
+	return visit2(graph, "svr", {}, { "dac", "fft" })
 end
 
 local content = utils.read_file("day_11.input")
 
+local graph = create_graph(content)
 utils.time(function()
-	print("Part 1: ", part_1(content))
+	print("Part 1: ", part_1(graph))
+end)
+utils.time(function()
+	print("Part 2: ", part_2(graph))
 end)
